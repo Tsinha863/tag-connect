f8e9fc3f0d0c86c81a0cec15ae118f9ea15c572f
TUSHAR SINHA <tsinha863@gmail.com>
2026-02-27 16:41:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3923750886-f8353",
+  "appId": "1:954679800070:web:05f274e8c2c8860ebbee87",
+  "apiKey": "AIzaSyCPuzT4PwNEID_9OAyQb1ELNLbS0A8jr3g",
+  "authDomain": "studio-3923750886-f8353.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "954679800070"
+};
