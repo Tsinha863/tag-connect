47f0cbf5315f1b501fd61c266b96823daad5b701
TUSHAR SINHA <tsinha863@gmail.com>
2026-02-27 16:32:17 +0000

    remove gemni

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/match-candidates-to-jobs-flow.ts';
-import '@/ai/flows/generate-job-description-flow.ts';
-import '@/ai/flows/recommend-jobs-to-students-flow.ts';
